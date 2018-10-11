b2e2f0625313bf44fd651d2971c0f344031f50aa
unknown <prabhakarupadhyay1997@gmail.com>
2018-10-11 18:18:53 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,27 @@
+var clickButton = document.getElementById("clickButton");
+var Start = document.getElementById("Start");
+var intervalRun = false;
+
+clickButton.onclick = function(e){
+	
+	if(intervalRun){
+		chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+			var activeTab = tabs[0];
+			chrome.tabs.sendMessage(activeTab.id, {"message": "true"});
+		});
+		intervalRun = false;
+		Start.innerHTML = "Start Again";
+		Start.style.left = "23px"
+		
+	}else{
+		Start.innerHTML = "Running..";
+		
+		chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+			var activeTab = tabs[0];
+			chrome.tabs.sendMessage(activeTab.id, {"message": "false"});
+		});
+		intervalRun = true;
+		
+	}
+};
+
